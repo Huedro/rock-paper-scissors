19f6a04016eec3e36fe09bb65e4e4fb1214fcf2a
Pedro Augusto <hpedroaugusto@hotmail.com>
2025-01-23 12:03:49 -0300

    Write logic for computer and human choice

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log("It works!");
+// Player write which of the 3 options he has 
+// Computer calls a function that randomly returns one of the 3 options 
+// The game tests who beats who
+// Show Victory or Defeat to the player
+
+console.log(getHumanChoice());
+
+function getComputerChoice() {
+    let randomNumber = Math.random() * 100;
+    
+    if (randomNumber >= 0 && randomNumber <= 33.33) {
+        return "rock";
+    } else if (randomNumber >= 33.33 && randomNumber <= 66.66) {
+        return "paper";
+    }
+    
+    return "scissors";
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    return humanChoice
+}
