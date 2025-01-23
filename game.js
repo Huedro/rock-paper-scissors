77356dbbd002e15e134cbffa413052686afdc5b7
Pedro Augusto <hpedroaugusto@hotmail.com>
2025-01-23 13:42:27 -0300

    Add play round

--- game.js
+++ game.js
@@ -3,7 +3,10 @@
 // The game tests who beats who
 // Show Victory or Defeat to the player
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice());
 
 function getComputerChoice() {
     let randomNumber = Math.random() * 100;
@@ -19,5 +22,27 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    return humanChoice
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === "rock" && computerChoice === "scissors" 
+        || humanChoice === "paper" && computerChoice === "rock" 
+        || humanChoice === "scissors" && computerChoice === "paper") 
+    {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock"
+     ) 
+     {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else {
+        console.log(`You two chose ${humanChoice}! It's a DRAW.`)
+    }
+
+    console.log(`SCORE: ${humanScore}x${computerScore}`);
 }
